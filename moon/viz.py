674af5728193092cb84a7947ad2d7de17a6a0658
Cove Geary <covertgeary@gmail.com>
2019-12-26 02:47:30 -0600

    Decent calculations for variable radius-size of hold visualizations

--- moon/viz.py
+++ moon/viz.py
@@ -1,10 +1,43 @@
+import numpy as np
 from PIL import Image, ImageDraw
 
+A = 128
+COLORS = dict(R=(255,0,0,A), G=(0,255,0,A), B=(0,0,255,A), P=(255,0,255,A))
 BG, FG = None, None
+
 HOLD_TOPLEFT = (93, 87)  # x, y in pixels; not row, col
 HOLD_SPACING = 50  # px
-A = 128
-COLORS = dict(R=(255,0,0,A), G=(0,255,0,A), B=(0,0,255,A), W=(255,255,255,A))
+MAX_RADIUS = 60
+MIN_RADIUS = 5
+
+def usage_to_image(usage, color, show=True):
+    assert (usage.shape == (18, 11)), "Usage array shape must be (18, 11)"
+    # Get min/max for pretty radius calculations
+    min_val, max_val = np.ma.masked_equal(usage, 0.0, copy=False).min(), usage.max()  # Nonzero minimum, maximum (https://stackoverflow.com/questions/7164397/find-the-min-max-excluding-zeros-in-a-numpy-array-or-a-tuple-in-python)
+    slope = (MAX_RADIUS - MIN_RADIUS) / (max_val - min_val)
+    # Variables for PIL
+    color = COLORS[color]
+    fg, bg = _imgs()
+    draw = ImageDraw.Draw(fg)
+    # Draw circles
+    for r in range(0, 18):
+        for c in range(0, 11):
+            val = usage[r, c]
+            if val:
+                rad = slope * (val - min_val) + MIN_RADIUS
+                coords = _rc_to_xy(r, c)
+                _circle(draw, coords, rad, color, fill=True)
+    # Return
+    img = Image.alpha_composite(bg, fg)
+    if show:
+        if get_ipython():
+            display(img)
+        else:
+            img.show()
+    return img
+
+def problem_to_image(p, show=False):
+    pass
 
 def _imgs(filename='mb2016.png'):
     global FG, BG
@@ -17,27 +50,8 @@ def _circle(draw, center, rad, color, fill=False):
     bounds = [center[0] - rad, center[1] - rad, center[0] + rad, center[1] + rad]
     if fill:
         draw.ellipse(bounds, fill=color)
-    else:
-        draw.ellipse(bounds, outline=color, width=3)
+    draw.ellipse(bounds, outline=(color[0], color[1], color[2], 255), width=2)
 
 def _rc_to_xy(r, c):
     origin_x, origin_y = HOLD_TOPLEFT
     return origin_x + c * HOLD_SPACING, origin_y + r * HOLD_SPACING
-
-def usage_to_image(usage, color, show=True):
-    assert (usage.shape == (18, 11)), "Usage array shape must be (18, 11)"
-    color = COLORS[color]
-    fg, bg = _imgs()
-    draw = ImageDraw.Draw(fg)
-    for r in range(0, 18):
-        for c in range(0, 11):
-            coord = _rc_to_xy(r, c)
-            if usage[r, c]:
-                _circle(draw, coord, 10, color, fill=True)
-    img = Image.alpha_composite(bg, fg)
-    if show:
-        img.show()
-    return img
-
-def problem_to_image(p, show=False):
-    pass
