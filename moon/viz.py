cb6c214b4a8fae36168ed27e13e87d334290421c
Cove Geary <covertgeary@gmail.com>
2019-12-26 03:13:28 -0600

    Refactor holds.ipynb->data.ipynb; implement missing method in viz.py

--- moon/viz.py
+++ moon/viz.py
@@ -1,8 +1,8 @@
 import numpy as np
 from PIL import Image, ImageDraw
 
-A = 128
-COLORS = dict(R=(255,0,0,A), G=(0,255,0,A), B=(0,0,255,A), P=(255,0,255,A))
+ALPHA = 128
+COLORS = dict(R=(255,0,0), G=(0,255,0), B=(0,0,255), P=(255,0,255))
 BG, FG = None, None
 
 HOLD_TOPLEFT = (93, 87)  # x, y in pixels; not row, col
@@ -26,7 +26,7 @@ def usage_to_image(usage, color, show=True):
             if val:
                 rad = slope * (val - min_val) + MIN_RADIUS
                 coords = _rc_to_xy(r, c)
-                _circle(draw, coords, rad, color, fill=True)
+                _circle(draw, coords, rad, color, fill=True, width=2)
     # Return
     img = Image.alpha_composite(bg, fg)
     if show:
@@ -36,8 +36,26 @@ def usage_to_image(usage, color, show=True):
             img.show()
     return img
 
-def problem_to_image(p, show=False):
-    pass
+def array_to_image(p, rad=30, show=False):
+    fg, bg = _imgs()
+    draw = ImageDraw.Draw(fg)
+    for r in range(0, 18):
+        for c in range(0, 11):
+            if not p[r, c, 3]:  # Not unused
+                if p[r, c, 0]:  # End
+                    color = 'R'
+                elif p[r, c, 1]:  # Start
+                    color = 'G'
+                elif p[r, c, 2]:  # Others
+                    color = 'B'
+                _circle(draw, _rc_to_xy(r, c), rad, COLORS[color], fill=False)
+    img = Image.alpha_composite(bg, fg)
+    if show:
+        if get_ipython():
+            display(img)
+        else:
+            img.show()
+    return img
 
 def _imgs(filename='mb2016.png'):
     global FG, BG
@@ -46,11 +64,11 @@ def _imgs(filename='mb2016.png'):
         FG = Image.new('RGBA', BG.size, (255, 255, 255, 0))
     return FG.copy(), BG
 
-def _circle(draw, center, rad, color, fill=False):
+def _circle(draw, center, rad, color, fill=False, width=5):
     bounds = [center[0] - rad, center[1] - rad, center[0] + rad, center[1] + rad]
     if fill:
-        draw.ellipse(bounds, fill=color)
-    draw.ellipse(bounds, outline=(color[0], color[1], color[2], 255), width=2)
+        draw.ellipse(bounds, fill=(color[0], color[1], color[2], ALPHA))
+    draw.ellipse(bounds, outline=(color[0], color[1], color[2], 255), width=width)
 
 def _rc_to_xy(r, c):
     origin_x, origin_y = HOLD_TOPLEFT
